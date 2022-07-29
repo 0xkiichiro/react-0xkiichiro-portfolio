5b293aa4a964d1d6ef3cb71065e764fd844068b9
0xkiichiro <barisaytimur@gmail.com>
2022-07-29 15:35:35 +0300

    projects fixed

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,16 +1,39 @@
 import ProjectCardStyled from "./ProjectCard.styled";
+import "./projectCard.css";
+import { AiFillGithub } from "react-icons/ai";
 
 const ProjectCard = ({ project }) => {
   return (
     <ProjectCardStyled>
-      <h2>{project.name}</h2>
+      <h2 className="project-card-heading">{project.name}</h2>
       <img
+        className="project-card-img"
         src={project.screenshot}
-        alt=""
+        alt="project-img"
         style={{ width: "525px", height: "200px" }}
       />
-      <p>{project.desc}</p>
-      <p>{project.tech}</p>
+      <div className="project-info">
+        <p className="project-desc">
+          {project.desc.length > 100
+            ? project.desc.substring(0, 150) + "..."
+            : project.desc}
+        </p>
+        <div className="project-tech-div">
+          <p>Tech used:</p>
+          <p className="project-tech">
+            {project.tech?.map((tech) => ` ${tech},`)}
+          </p>
+        </div>
+        <div className="project-github-div">
+          <p>To check the source code =&gt;</p>
+          <a className="contact-link" href={project.github} target="_blank">
+            <AiFillGithub className="project-github-icon" />
+          </a>
+        </div>
+        <a className="live-site-link" href={project.liveSite} target="_blank">
+          Click here for the live site!
+        </a>
+      </div>
     </ProjectCardStyled>
   );
 };
--- src/components/project-card/ProjectCard.styled.jsx
+++ src/components/project-card/ProjectCard.styled.jsx
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 const ProjectCardStyled = styled.div`
   background-color: #2b2b2b;
-  height: 400px;
+  height: 450px;
   width: 525px;
+  border-radius: 0.8rem;
+  overflow: hidden;
 `;
 
 export default ProjectCardStyled;
